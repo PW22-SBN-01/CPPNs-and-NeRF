77b2e0f150750c9a3784cefada09d41cc4304ae6
AdityaNG <adityang5@gmail.com>
2022-02-03 09:37:40 +0530

    Normal Dist model added

--- rand.py
+++ rand.py
@@ -1,9 +1,7 @@
-import datetime
-import tensorflow as tf
-from tensorflow import keras
-
+from operator import mod
 import os
 import math
+from statistics import mode
 import cv2
 import urllib
 import numpy as np
@@ -13,6 +11,12 @@ from io import BytesIO
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 
+#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+import datetime
+import tensorflow as tf
+from tensorflow import keras
+
 url = 'https://wallpaperaccess.com/full/760289.jpg' # 514.02K kB (jpg)
 response = requests.get(url)
 img = Image.open(BytesIO(response.content))
@@ -21,17 +25,23 @@ data = cv2.resize(data, (0, 0), fx = 0.1, fy = 0.1) # 126.80 kB (png)
 
 #cv2.imwrite("original.png", cv2.cvtColor(data, cv2.COLOR_BGR2RGB))
 
-#for i in range(2, 10):
-for i in range(5, 10):
-    L_FACT = 2**i +1
-    print("L_FACT=", L_FACT)
+def run_training(SIGMA, NUM):
+    #SIGMA = i
+    #NUM = j
+    print("SIGMA=", SIGMA, "NUM=",NUM)
+    np.random.seed(0)
+    B = np.random.normal(0, SIGMA**0.5, size=NUM).reshape((NUM, 1))
+    #print(B)
 
-    # Explode the input vector V into [sin(1.V),  sin(2.V), sin(3.V), sin(4.V), ... , sin((L-1).V)]
-    def explode(xi, yi):
-        v = np.array([xi, yi]).reshape((2,1)).T
-        mul = np.arange(1, L_FACT).reshape((L_FACT-1, 1))
-        #mul = np.power(2, np.arange(0, L_FACT)).reshape((L_FACT, 1))
-        return  np.sin(np.matmul(mul, v)).flatten()
+    # Explode the input vector v into [sin(B.v),  cos(B.v)] such that B is normal distribution N(0, SIGMA) with NUM elements
+    def explode(xi, yi, B=B):
+        v = np.array([xi, yi]).reshape((2,1))
+        
+        mul = np.matmul(B, v.T)
+        
+        s = np.sin(mul).flatten()
+        c = np.cos(mul).flatten()
+        return np.concatenate((s,c))
 
     # Dataset generation from the image
     X, y = [], []
@@ -54,33 +64,45 @@ for i in range(5, 10):
 
     model.compile(optimizer = 'adam', loss=keras.metrics.mean_squared_error, metrics = ['mse'])
     model.summary()
-    keras.utils.plot_model(
-    model, to_file='imgs/model_' + str(L_FACT) + '.png', show_shapes=True, show_dtype=True,
+    """
+    keras.utils.plot_model( model, to_file='imgs/model_'+ str(SIGMA)) + "_" + str(NUM) + '.png', show_shapes=True, show_dtype=True,
         show_layer_names=True, rankdir='TB', expand_nested=False, dpi=96,
         layer_range=None, show_layer_activations=True
     )
-
+    """
     
     # Model Checkpoint Definition
-    MODEL_SAVE_PATH = os.path.join('models', "model_" + str(L_FACT))
+    MODEL_NAME = "model_" + str(SIGMA)+ "_" + str(NUM)
+    MODEL_SAVE_PATH = os.path.join('models',  MODEL_NAME)
     CHECKPOINT_SAVE_PATH = os.path.join(MODEL_SAVE_PATH, 'checkpoints')
+    MODEL_H5_SAVE_PATH = os.path.join(MODEL_SAVE_PATH, MODEL_NAME + ".h5")
     IMAGE_SAVE_PATH = os.path.join(MODEL_SAVE_PATH, 'imgs')
+    B_SAVE_PATH = os.path.join(MODEL_SAVE_PATH, 'B.npy')
     
     os.makedirs(MODEL_SAVE_PATH, exist_ok=True)
     os.makedirs(CHECKPOINT_SAVE_PATH, exist_ok=True)
     os.makedirs(IMAGE_SAVE_PATH, exist_ok=True)
 
+    np.save(B_SAVE_PATH, B)
+
     model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
         filepath=CHECKPOINT_SAVE_PATH,
         save_weights_only=False,
-        monitor='val_accuracy',
-        mode='max',
+        monitor='val_loss',
+        mode='min',
         save_best_only=True
     )
 
     class CustomCallback(keras.callbacks.Callback):
+        def __init__(self) -> None:
+            super().__init__()
+            self.best_val_loss = float('inf')
         def on_epoch_end(self, epoch, logs=None):
-            keys = list(logs.keys())
+
+            if logs['val_loss'] < self.best_val_loss:
+                self.best_val_loss = logs['val_loss']
+                self.model.save(MODEL_H5_SAVE_PATH, overwrite=True)
+
             if epoch%1==0:
                 self.draw_img(self.model, data)
 
@@ -108,13 +130,28 @@ for i in range(5, 10):
 
             IMG_PATH = os.path.join(IMAGE_SAVE_PATH, str(datetime.datetime.now()) + '.png')
             #cv2.imwrite(IMG_PATH, res)
-            cv2.imwrite(IMG_PATH, cv2.cvtColor(res, cv2.COLOR_BGR2RGB))
+            #cv2.imwrite(IMG_PATH, cv2.cvtColor(res, cv2.COLOR_BGR2RGB))
+            cv2.imwrite(IMG_PATH, cv2.cvtColor(res.copy(), cv2.COLOR_BGR2RGB))
 
 
     # Model Training
     #model.fit(X, y, epochs=55, validation_data=(X_test, y_test), callbacks=[CustomCallback(), model_checkpoint_callback])
-    #model.fit(X, y, epochs=55, validation_data=(X, y), callbacks=[CustomCallback(), model_checkpoint_callback])
+    model.fit(X, y, epochs=25, validation_data=(X, y), callbacks=[CustomCallback(), model_checkpoint_callback])
 
     # Clean up
-    del model, X, y, X_train, X_test, y_train, y_test
+    # del model, X, y, X_train, X_test, y_train, y_test
     print("-"*30)
+    """
+    Usage:
+    p = [(x1,y1), (x2, y2), ... , (xn, yn)] # list of query points
+    v = list(map(lambda pi: explode(pi[0], pi[1], B), p)) # input vector
+    predictions = model.predict(v) # Model predictions
+    """
+    return model, B, explode
+
+if __name__=="__main__":
+    run_training(200, 256)
+    exit()
+    for SIGMA in range(1, 500, 10):
+        for NUM in range(5, 40, 5):
+            pass
