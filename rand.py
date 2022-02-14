7c5e30897901dde85fdae808ff8902dcf4ba2c84
AdityaNG <adityang5@gmail.com>
2022-02-14 12:59:44 +0530

    Experiments

--- rand.py
+++ rand.py
@@ -17,11 +17,26 @@ import datetime
 import tensorflow as tf
 from tensorflow import keras
 
-url = 'https://wallpaperaccess.com/full/760289.jpg' # 514.02K kB (jpg)
-response = requests.get(url)
-img = Image.open(BytesIO(response.content))
-data = np.array(img)
-data = cv2.resize(data, (0, 0), fx = 0.1, fy = 0.1) # 126.80 kB (png)
+import sys
+
+sys.path.append(os.path.join(os.path.curdir, 'kitti-utils'))
+from data_parser import KittiDataParser
+from calibration import Calibration
+
+KITTI_PATH = "/mnt/HDD/home/aditya/kitti/kitti_data/"
+KITTI_EXAMPLE = os.path.join(KITTI_PATH, "2011_09_26/2011_09_26_drive_0001_sync/")
+KITTI_CALIB_CAM2CAM = os.path.join(KITTI_PATH, "2011_09_26/calib_cam_to_cam.txt")
+KITTI_CALIB_IMU2VEL = os.path.join(KITTI_PATH, "2011_09_26/calib_imu_to_velo.txt")
+KITTI_CALIB_VEL2CAM = os.path.join(KITTI_PATH, "2011_09_26/calib_velo_to_cam.txt")
+
+kitti = KittiDataParser(KITTI_EXAMPLE, file_mode=1)
+calib = Calibration(KITTI_CALIB_CAM2CAM)
+
+img_arr = kitti.load_image("0000000000")
+
+for i in range(len(img_arr)):
+    img_arr[i] = cv2.resize(img_arr[i].copy(), (0, 0), fx = 0.1, fy = 0.1) 
+    #img_arr[i] = cv2.cvtColor(img_arr[i].copy(), cv2.COLOR_BGR2RGB)
 
 #cv2.imwrite("original.png", cv2.cvtColor(data, cv2.COLOR_BGR2RGB))
 
@@ -34,8 +49,9 @@ def run_training(SIGMA, NUM):
     #print(B)
 
     # Explode the input vector v into [sin(B.v),  cos(B.v)] such that B is normal distribution N(0, SIGMA) with NUM elements
-    def explode(xi, yi, B=B):
-        v = np.array([xi, yi]).reshape((2,1))
+    def explode(P, B=B):
+        xi, yi, zi = P[0,0], P[1,0], P[2,0]
+        v = np.array([xi, yi, zi]).reshape((3,1))
         
         mul = np.matmul(B, v.T)
         
@@ -45,10 +61,16 @@ def run_training(SIGMA, NUM):
 
     # Dataset generation from the image
     X, y = [], []
-    for i in range(data.shape[0]):
-        for j in range(data.shape[1]):
-            X.append( explode(i, j) )
-            y.append( data[i,j] / 256.0 )
+    for img_i in range(len(img_arr)):
+        cam_name = '0' + str(img_i)
+        R = calib.calib['R_' + cam_name].reshape((3,3))
+        T = calib.calib['T_' + cam_name].reshape((3,1))
+        data = img_arr[img_i]
+        for i in range(data.shape[0]):
+            for j in range(data.shape[1]):
+                point = np.array([i, j, 0]).reshape((3,1))
+                X.append( explode(np.matmul(R, point) + T) )
+                y.append( data[i,j] / 256.0 )
 
     X = np.array(X)
     y = np.array(y)
@@ -104,7 +126,13 @@ def run_training(SIGMA, NUM):
                 self.model.save(MODEL_H5_SAVE_PATH, overwrite=True)
 
             if epoch%1==0:
-                self.draw_img(self.model, data)
+                for img_i in range(len(img_arr)):
+                    cam_name = '0' + str(img_i)
+                    R = calib.calib['R_' + cam_name].reshape((3,3))
+                    T = calib.calib['T_' + cam_name].reshape((3,1))
+                    data = img_arr[img_i]
+                    
+                    self.draw_img(self.model, data, R, T, cam_name)
 
         def draw_img(self, model, data):
             xi = np.arange(0, data.shape[0])
@@ -114,11 +142,14 @@ def run_training(SIGMA, NUM):
         
             preds = np.transpose([np.tile(xi, len(yi)), np.repeat(yi, len(xi))])
 
+
             v = []
             for i in range(len(preds)):
                 #tmpxi, tmpyi = np.round((preds[i][0]+1)*data.shape[0]/2.0), np.round((preds[i][1]+1)*data.shape[1]/2.0)
                 tmpxi, tmpyi = np.round(preds[i][0]), np.round(preds[i][1])
-                v.append(explode(tmpxi, tmpyi))
+                point = np.array([tmpxi, tmpyi, 0]).reshape((3,1))
+                v.append( explode(np.matmul(R, point) + T) )
+                #v.append(explode(tmpxi, tmpyi))
             v = np.array(v)
 
             mod_preds = model.predict(v)
